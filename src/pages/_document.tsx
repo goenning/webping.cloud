b5c0450c63fd543c4f729480b714076430dd7199
goenning <me@goenning.net>
2020-10-26 21:21:37 +0000

    same-origin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,6 +28,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {googleAnalyticsScripts}
+          <meta name="referrer" content="same-origin" />
           <link rel="icon" href="favicon.ico" type="image/x-icon" />
           <link rel="icon" href="favicon-16x16.png" sizes="16x16" type="image/png" />
           <link rel="icon" href="favicon-32x32.png" sizes="32x32" type="image/png" />
