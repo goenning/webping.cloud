ab94e1db35beea6c079b8683295a9f581985a0fa
goenning <me@goenning.net>
2024-02-04 18:46:18 +0000

    remove ga

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/p.js',
+        destination: 'https://plausible.io/js/plausible.js',
+      },
+      {
+        source: '/e',
+        destination: 'https://plausible.io/api/event',
+      },
+    ]
+  },
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,26 +8,9 @@ export default class MyDocument extends Document {
   }
 
   render(): JSX.Element {
-    const googleAnalyticsScripts = process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
-      <>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
-          `,
-          }}
-        />
-      </>
-    )
-
     return (
       <Html lang="en">
         <Head>
-          {googleAnalyticsScripts}
           <meta name="referrer" content="same-origin" />
           <link rel="icon" href="favicon.ico" type="image/x-icon" />
           <link rel="icon" href="favicon-16x16.png" sizes="16x16" type="image/png" />
@@ -36,6 +19,7 @@ export default class MyDocument extends Document {
         <body className="bg-gray-200 text-gray-900">
           <Main />
           <NextScript />
+          <script defer data-domain="webping.cloud" data-api="/e" src="/p.js"></script>
         </body>
       </Html>
     )
